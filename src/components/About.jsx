56d0dcfd8e9183ffec8e8763b410e2e1c2d5f818
Southern-j <jonatan.h.bergquist@gmail.com>
2024-12-09 15:57:18 +0100

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <section id="about">
       <h2 className="text-2xl font-bold mb-4 tracking-tight text-gray-800">About "just doing things"</h2>
-      <p className="text-base leading-relaxed tracking-wide text-gray-700">As most of those who even do realize it, they realizes too late that you can choose to <i>just do things</i>. For example build a personal landing page.</p>
+      <p className="text-base leading-relaxed tracking-wide text-gray-700">As most of those who even do realize it, they realize too late that you can choose to <i>just do things</i>. For example build a personal landing page, invest into startups or build a defense tech community.</p>
     </section>
   );
 };
