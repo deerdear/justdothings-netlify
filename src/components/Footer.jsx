398cd1de5ca06aa1f2e73dd456bb1929a4c02c41
Southern-j <jonatan.h.bergquist@gmail.com>
2025-04-21 12:51:15 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="mt-12 text-center">
-      <p className="text-sm tracking-wide text-gray-700">&copy; 2024 jlb. All rights reserved.</p>
+      <p className="text-sm tracking-wide text-gray-700">&copy; 2025 jlb. All rights reserved.</p>
     </footer>
   );
 };
